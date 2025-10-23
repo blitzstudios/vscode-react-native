571fcc23f030e8cf1c5d30ce6f76704f38247cf5
Luke Herbold <luke@sleeper.app>
2025-10-23 16:28:58 -0700

    Add packager args!

--- src/common/packager.ts
+++ src/common/packager.ts
@@ -105,6 +105,10 @@ export class Packager {
         return this.expoHelper;
     }
 
+    public getPackagerArguments(): string[] {
+        return SettingsHelper.getPackagerArgs(this.workspacePath);
+    }
+
     public getPort(): number {
         return this.packagerPort || SettingsHelper.getPackagerPort(this.workspacePath);
     }
@@ -185,6 +189,11 @@ export class Packager {
 
         if (resetCache) {
             args = args.concat("--resetCache");
+        }
+
+        const additionalArgs = this.getPackagerArguments();
+        if (additionalArgs) {
+            args = args.concat(additionalArgs);
         }
 
         const isExpo = await this.getExponentHelper().isExpoManagedApp(false);
